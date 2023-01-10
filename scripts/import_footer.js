b6f33bd6a83e39534fdfbb08a37e64300e4deeb9
ausmull22 <71801580+ausmull22@users.noreply.github.com>
2023-01-10 15:42:41 -0500

    Added Button Animations/Zodiac Lottie Template

---
+++ scripts/import_footer.js
@@ -0,0 +1,3 @@
+$(function(){
+      $("#includedContent").load("b.html"); 
+});
--- scripts/zodiac_animation.js
+++ scripts/zodiac_animation.js
@@ -5,3 +5,20 @@ var animation = bodymovin.loadAnimation({
     autoplay: true,
     path: 'lottie/opening_logo.json'
 })
+
+var animation_2 = bodymovin.loadAnimation({
+    container: document.getElementById('input_field_animation'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'lottie/opening_logo.json'
+})
+
+var animation_aries = bodymovin.loadAnimation({
+    container: document.getElementById('selected_zodiac_aries'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'lottie/virgo.json'
+})
+
