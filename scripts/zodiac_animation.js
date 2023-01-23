4c0f2bc4c24e7f39710c917de8d0202db0296186
ausmull22 <71801580+ausmull22@users.noreply.github.com>
2023-01-22 20:03:50 -0500

    Readjusting Margins/Padding

--- scripts/zodiac_animation.js
+++ scripts/zodiac_animation.js
@@ -1,18 +1,3 @@
-var animation = bodymovin.loadAnimation({
-    container: document.getElementById('anim_mobile'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'lottie/opening_logo.json'
-})
-
-var animation_2 = bodymovin.loadAnimation({
-    container: document.getElementById('input_field_animation'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'lottie/opening_logo.json'
-})
 
 var sunrise_loading = bodymovin.loadAnimation({
     container: document.getElementById('sunrise_loading'),
